4386928224bf029a83aef78fb7a7f910ac191979
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2024-06-06 17:42:11 +0500

    Update table_checker.ex

--- lib/t_s/repository/table_checker.ex
+++ lib/t_s/repository/table_checker.ex
@@ -8,7 +8,7 @@ defmodule TS.Repository.TableChecker do
 
   def new_table_dates(start_date, table_prefix) do
     date_now = Calendar.strftime(start_date, "20%y_%m")
-    date_prev = Calendar.strftime(NaiveDateTime.new!(year_handler(start_date.year, start_date.month), month_handler(start_date.month), 15, 12, 12, 12), "20%y_%m")
+    date_prev = Calendar.strftime(NaiveDateTime.new!(year_handler(start_date.year, start_date.month, start_date.day), month_handler(start_date.month, start_date.day), 15, 12, 12, 12), "20%y_%m")
     if Ecto.Adapters.SQL.table_exists?(TS.Repo, table_prefix <> date_prev) and Ecto.Adapters.SQL.table_exists?(TS.Repo, table_prefix <> date_now) do
       [date_now, date_prev]
     else
@@ -37,19 +37,35 @@ defmodule TS.Repository.TableChecker do
     end
   end
 
-  defp year_handler(prev_year, 1) do
+  defp year_handler(prev_year, 1, day) when day < 14 do
     prev_year - 1
   end
 
-  defp year_handler(prev_year, _) do
+  defp year_handler(prev_year, _, day) when day < 14 do
     prev_year
   end
 
-  defp month_handler(1) do
+  defp year_handler(next_year, 12, _day) do
+    next_year + 1
+  end
+
+  defp year_handler(next_year, _, _day) do
+    next_year
+  end
+
+  defp month_handler(1, day) when day < 14 do
     12
   end
 
-  defp month_handler(prev_month) do
+  defp month_handler(prev_month, day) when day < 14 do
     prev_month - 1
+  end
+
+  defp month_handler(12, _day) do
+    1
+  end
+
+  defp month_handler(next_month, _day) do
+    next_month + 1
   end
 end
