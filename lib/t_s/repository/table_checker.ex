2d6294ed136f8542a94f17bebf50c3bdd24ee50a
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2024-06-25 10:34:17 +0500

    Update table_checker.ex

--- lib/t_s/repository/table_checker.ex
+++ lib/t_s/repository/table_checker.ex
@@ -5,6 +5,7 @@ defmodule TS.Repository.TableChecker do
       "CREATE TABLE IF NOT EXISTS #{table} (#{schema_text})"
     )
   end
+
   def new_table_dates(start_date, table_prefix) do
     date_now = Calendar.strftime(start_date, "20%y_%m")
     date_prev = Calendar.strftime(NaiveDateTime.new!(year_handler(start_date.year, start_date.month, start_date.day), month_handler(start_date.month, start_date.day), 15, 12, 12, 12), "20%y_%m")
@@ -18,7 +19,9 @@ defmodule TS.Repository.TableChecker do
       end
     end
   end
+
   def all_table_dates(start_date, table_prefix, schema_text, table_list \\ [], not_repeat \\ true)
+
   def all_table_dates(start_date, table_prefix, schema_text, table_list, true) do
     date_now = Calendar.strftime(start_date, "20%y_%m")
     check(table_prefix <> date_now, schema_text)
@@ -33,27 +36,35 @@ defmodule TS.Repository.TableChecker do
       table_list
     end
   end
+
   defp year_handler(prev_year, 1, day) when day < 14 do
     prev_year - 1
   end
+
   defp year_handler(prev_year, _, day) when day < 14 do
     prev_year
   end
+
   defp year_handler(next_year, 12, _day) do
     next_year + 1
   end
+
   defp year_handler(next_year, _, _day) do
     next_year
   end
+
   defp month_handler(1, day) when day < 14 do
     12
   end
+
   defp month_handler(prev_month, day) when day < 14 do
     prev_month - 1
   end
+
   defp month_handler(12, _day) do
     1
   end
+
   defp month_handler(next_month, _day) do
     next_month + 1
   end
